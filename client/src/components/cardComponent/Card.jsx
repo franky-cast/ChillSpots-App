f25d507e48a55dede61aeeb6470e2adcd0bd836a
Francisco Castillo <frankycstl@gmail.com>
2023-07-27 13:32:45 -0700

    frontend components update

--- client/src/components/cardComponent/Card.jsx
+++ client/src/components/cardComponent/Card.jsx
@@ -1,50 +1,24 @@
-import { useState, useEffect } from "react"
 import "./card.css"
-import fetchLocations from "../../api/location"
 import star from "../../../public/assets/star.png"
 
-
-function Card () {
-    const [ locations, setLocations ] = useState([])
-
-    useEffect(() => {
-        const fetchLocationsData = async () => {
-            try {
-                const data = await fetchLocations()
-                setLocations(data)
-            } catch (err) {
-                console.error(`Error fetching location data: ${err}`)
-            }
-        }
-        fetchLocationsData()
-    }, [])
-
-    const data = locations.map( (location) => {
-        const coverImg = location.pictures[0]
-        return (
-            location.approved?
-            <a className="card--a-tag" href="#">
-                <div className="card">
-                    <img src={coverImg} alt={location.name} className="card--image"></img>
-                    <div className="card--info">
-                        <img src={star} alt="Star icon" className="star-png"></img>
-                        {/* <p className="card--rating">{location.stats.rating}</p> */}
-                        {/* <p className="card--reviews gray">({location.stats.reviewCount})</p> */}
-                        <p className="card--country gray">{location.city}</p>
-                    </div>
-                    <p className="card--title">{location.title}</p>
-                    {/* <p className="card--price"> <strong>From ${location.price}</strong> /person</p> */}
-                </div>
-            </a>
-            :
-            null
-        )
-    })
+function Card (props) {
+    const { name, pictures, stats, address, city, state, latitude, longtitude, approved, timestamp } = props.location
+    const coverImg = pictures[0]
 
     return (
-        <ul>
-            {data}
-        </ul>
+        <a className="card--a-tag" href="#">
+            <div className="card">
+                <img src={coverImg} alt={`image of ${name}`} className="card--image"></img>
+                <div className="card--info">
+                    <img src={star} alt="Star icon" className="star-png"></img>
+                    {/* <p className="card--rating">{location.stats.rating}</p> */}
+                    {/* <p className="card--reviews gray">({location.stats.reviewCount})</p> */}
+                    <p className="card--country gray">{city}</p>
+                </div>
+                <p className="card--title">{name}</p>
+                {/* <p className="card--price"> <strong>From ${location.price}</strong> /person</p> */}
+            </div>
+        </a>
     )
 }
 
--- client/src/components/locationsComponent/Locations.jsx
+++ client/src/components/locationsComponent/Locations.jsx
@@ -1,11 +1,29 @@
 import './locations.css'
 import Card from '../cardComponent/Card'
+import fetchLocations from "../../api/location"
+import { useState, useEffect } from "react"
 
 function Locations () {
-    
+    const [ locations, setLocations ] = useState([])
+
+    useEffect(() => {
+        const fetchLocationsData = async () => {
+            try {
+                const data = await fetchLocations()
+                setLocations(data)
+            } catch (err) {
+                console.error(`Error fetching location data: ${err}`)
+            }
+        }
+        fetchLocationsData()
+    }, [])
+
+    const data = locations.map(location => (location.approved && <Card key={location._id} location= {location}/>) )
+
     return (
         <div className='container'>
             <p>Locations component</p>
+            <div> {data} </div>
         </div>
     )
 }
