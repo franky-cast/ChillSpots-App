a5317deb698c7972350a8b70abd677bec67e47db
Cole Simpson <cole22simpson@gmail.com>
2023-08-02 01:31:56 -0700

    nav looks a bit better

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import "./nav.css"
 import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
 import logo from "/assets/logos/footerlogo.png"
+import menu from "/assets/utilities/hamburger.jpeg"
 import * as FaIcons from 'react-icons/fa';
 import * as FaIcons6 from 'react-icons/fa6';
 
@@ -17,60 +18,62 @@ function Nav() {
     };
     
     return (
-      <>
         <IconContext.Provider value={{ color: 'black' }}>
             <div className='navbar'>
-                <Link to='#' className='hamburger-menu'>
-                    <FaIcons.FaBars onClick={showSidebar} />
+                <Link to='#' className='hamburger-menu-wrap'>
+                    <img
+                        src={menu}
+                        onClick={showSidebar}
+                        alt='hamburger menu'
+                        className='hamburger-menu-img' />
                 </Link>
                 <div className="img-wrap">
-                <img
-                    src={logo}
-                    alt="chill spots"
-                    className="img"
-                />
+                    <img
+                        src={logo}
+                        alt="chill spots"
+                        className="img"
+                    />
                 </div>
             </div>
         
     
             <nav className={sidebar ? 'nav active' : 'nav'} style={{ zIndex: 99 }}>
-                <ul className='nav-items' onClick={showSidebar}>
-                  <li className='navbar-toggle'>
-                    <Link to='#' className='hamburger-menu'>
-                      <FaIcons6.FaXmark />
-                    </Link>
-                  </li>
-                  <li className="nav-text">
-                    <Link to='/'>
-                        <FaIcons.FaSearch className='nav-icon' />
-                      <span>Search</span>
-                    </Link>
-                  </li>
-                  
-                  <li className="nav-text">
-                    <Link to='/discover'>
-                        <FaIcons.FaGlobeAmericas className='nav-icon' />
-                      <span>Discover</span>
-                    </Link>
-                  </li>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='hamburger-menu-wrap'>
+                            <FaIcons6.FaXmark />
+                        </Link>
+                    </li>
+                    <li className="nav-text">
+                        <Link to='/'>
+                            <FaIcons.FaSearch className='nav-icon' />
+                            <span>Search</span>
+                        </Link>
+                    </li>
+                    
+                    <li className="nav-text">
+                        <Link to='/discover'>
+                            <FaIcons.FaGlobeAmericas className='nav-icon' />
+                            <span>Discover</span>
+                        </Link>
+                    </li>
 
-                  <li className="nav-text">
-                    <Link to='/saved'>
-                        <FaIcons.FaBookmark className='nav-icon' />
-                      <span>Saved</span>
-                    </Link>
-                  </li>
+                    <li className="nav-text">
+                        <Link to='/saved'>
+                            <FaIcons.FaBookmark className='nav-icon' />
+                            <span>Saved</span>
+                        </Link>
+                    </li>
 
-                  <li className="nav-text">
-                    <Link to='/profile'>
-                        <FaIcons6.FaCircleUser className='nav-icon' />
-                      <span>Profile</span>
-                    </Link>
-                  </li>
-              </ul>
+                    <li className="nav-text">
+                        <Link to='/profile'>
+                            <FaIcons6.FaCircleUser className='nav-icon' />
+                            <span>Profile</span>
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         </IconContext.Provider>
-        </>
     );
 }
 
