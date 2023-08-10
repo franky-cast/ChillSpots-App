feffd369d7c050770393e221c6d241973598a25a
Francisco Castillo <frankycstl@gmail.com>
2023-08-10 12:00:47 -0700

    updated Login component --> styling and responsiveness

--- client/src/components/home/Searchbar.jsx
+++ client/src/components/home/Searchbar.jsx
@@ -4,8 +4,8 @@ import svgArrow from "/assets/utilities/icon-arrow.svg"
 function Searchbar () {
     return (
         <form className="form d-flex" action="#">
-            <input id="input-el" className="form__input" type="text" placeholder="search by spot name" />
-            <button id="button" className='button'><img className="svg-arrow" src={svgArrow} alt="Search button" /></button>
+            <input id="input-el" className="searchbar__input" type="text" placeholder="search by spot name" />
+            <button id="button" className='searchbar__button'><img className="svg-arrow" src={svgArrow} alt="Search button" /></button>
         </form>
     )
 }
