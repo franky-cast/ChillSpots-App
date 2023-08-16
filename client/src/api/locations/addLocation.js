cd7eb7bb65bed2781981875e142b5f37a6cad072
Francisco Castillo <frankycstl@gmail.com>
2023-08-16 12:54:41 -0700

    updated client api --> function to call '/locations/add' backend route

--- client/src/api/locations/addLocation.js
+++ client/src/api/locations/addLocation.js
@@ -1,30 +1,14 @@
 import axios from "axios"
 
-async function addLocation (locationData, fork) {
-
-    if (fork === 'byAddress') {
-        try {
-		    const res = await axios.post(`${import.meta.env.VITE_APP_URL}/locations/addByAddress`, {
-                locationData: locationData
-            })
-		    return res
-	    } catch (err) {
-		    return res.json(`Internal server error: ${err}`)
-	    }
+async function addLocation (locationData) {
+    try {
+	    const res = await axios.post(`${import.meta.env.VITE_APP_URL}/locations/add`, {
+            locationData: locationData
+        })
+	    return res
+	} catch (err) {
+	    return res.json(`Internal server error: ${err}`)
     }
-
-
-    if (fork === 'latlng') {
-        try {
-		    const res = await axios.post(`${import.meta.env.VITE_APP_URL}/locations/add`, {
-                locationData: locationData
-            })
-		    return res
-	    } catch (err) {
-		    return res.json(`Internal server error: ${err}`)
-	    }
-    }
-	
 }
 
 export default addLocation
